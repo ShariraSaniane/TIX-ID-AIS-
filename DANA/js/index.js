efd2054a9b744abbe7d6b3e8208e997ebfe5919b
Sarah H <sarahpontoh02@gmail.com>
2022-12-07 08:53:53 +0700

    profile done

--- DANA/js/index.js
+++ DANA/js/index.js
@@ -1,11 +1,29 @@
 window.localStorage.clear();
 
 // api url
-const moneygo = "https://moneygo-api.herokuapp.com/api/";
+const dana = "https://dana-api.glitch.me/api/";
 
 //get
-fetch(moneygo)
+fetch(dana)
 .then((response)=>response.json())
 .then((json)=>console.log(json))
 
+
+// var dana = "https://dana-api.glitch.me/api/";
+
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+// };
+
+// async function getResponse(){
+//     try {
+//         let res = await fetch(dana, requestOptions)
+//         console.log("Fetch berhasil");
+//         window.localStorage.clear();
+//         return await (res.text());
+//     } catch(error) {
+//         console.log('error', error)
+//     };
+// }
 
