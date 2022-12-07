5573a985c6b5950e383f38b632d1437d383495ec
Sarah H <sarahpontoh02@gmail.com>
2022-12-07 10:26:18 +0700

    transfer

--- DANA/js/transfer.js
+++ DANA/js/transfer.js
@@ -1,64 +1,20 @@
 //ambil token dari local.Storage. local storage ada di page source >> application
-const moneygo = JSON.stringify(localStorage.getItem('moneygo'));
-// const ecia = JSON.stringify(localStorage.getItem('ecia'));
-const harpay = JSON.stringify(localStorage.getItem('harpay'));
-const coinless = JSON.stringify(localStorage.getItem('coinless'));
+const dana = JSON.stringify(localStorage.getItem('dana'));
 
-
-const id_penerima = document.querySelector("#id_penerima"); //id penerima coinless
-const hp = document.querySelector("#hp"); //hp penerima harpay
-const email = document.querySelector("#email"); //receiver's email moneygo
+const hp_penerima = document.querySelector("#no_hp"); // receiver's phone
 const balance = document.querySelector("#nominal");
-const emoney = document.querySelector("#emoney"); 
-const pin = document.querySelector("#pin"); //pin harpay
 const buttonSubmit = document.querySelector("#submit");
 
 buttonSubmit.addEventListener("click", (e) => {
     e.preventDefault(); 
 
-    var selected = emoney.options[emoney.selectedIndex].value; //selected value from the drop down
-  
-    if(selected == "Moneygo"){
-        var api_transfer = "https://moneygo-api.herokuapp.com/api/transfer";
-        var token = ("Bearer " + moneygo).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
-        var method = "PUT";
+    var api_transfer = "https://dana-api.glitch.me/api/transfer";
+    var token = ("Bearer " + dana).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
 
-        var raw = JSON.stringify({
-            email: email.value,
-            balance: balance.value
-        });
-
-    }
-    if(selected == "Coinless"){
-        var parsed_harga = parseInt(balance.value);
-        // console.log(total_harga);
-        // console.log(pin.value);
-
-        var api_transfer = "https://coinless.herokuapp.com/api/transfer";
-        var token = ("Bearer " + coinless).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
-        var method = "POST";
-
-        var raw = JSON.stringify({
-            tujuan:id_penerima.value,
-            jumlah: parsed_harga
-        });
-        
-    }
-    if(selected == "Harpay"){
-        var parsed_pin = parseInt(pin.value);
-        // console.log(total_harga);
-        console.log(pin.value);
-
-        var api_transfer = " https://harpay-api.herokuapp.com/transaksi/transferSaldo";
-        var token = ("Bearer " + harpay).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
-        var method = "POST";
-
-        var raw = JSON.stringify({
-        noTelp: hp.value,
-        nominal: balance.value,
-        pin: parsed_pin
+    var raw = JSON.stringify({
+        receiver_hp: hp_penerima.value,
+        balance: balance.value
     });
-    }
 
     var myHeaders = new Headers();
     myHeaders.append("Authorization", token);
@@ -66,7 +22,7 @@ buttonSubmit.addEventListener("click", (e) => {
     
 
     var requestOptions = {
-        method: method,
+        method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
@@ -92,9 +48,9 @@ buttonSubmit.addEventListener("click", (e) => {
         var resp_api = JSON.parse(data_api);
 
         //kalo success
-        if(resp_api.status == 200 || resp_api.message == "Successfully payment for transfer saldo"){
+        if(resp_api.status == 200){
             alert(resp_api.message);
-            window.location.href = "home.php";
+            window.location.href = "profile.html";
         }else{
             alert(resp_api.message);
         }
